c0e2afd788dc3804806d684eca11201d9b8f60fd
VS <143146645+vswe16@users.noreply.github.com>
2026-01-03 23:19:41 +0900

    Update test_decision_tree_pricer.py

--- Custom Machine Learning Package/tests/test_decision_tree_pricer.py
+++ Custom Machine Learning Package/tests/test_decision_tree_pricer.py
@@ -1,12 +1,1 @@
-import numpy as np
-from airbnb_pricing_package.decision_tree_pricer import DecisionTreePricer
 
-def test_decision_tree_predict():
-    X = np.array([[1], [2], [3], [4]])
-    y = np.array(["low", "low", "high", "high"])
-
-    model = DecisionTreePricer(max_depth=2)
-    model.train(X, y)
-
-    pred = model.predict([[1.5]])
-    assert pred[0] in ["low", "high"]
