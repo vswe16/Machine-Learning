637d1f71b195f63e3fd6c54bcd4d4205335eca8f
VS <143146645+vswe16@users.noreply.github.com>
2026-01-03 23:20:11 +0900

    Update test_neural_network_pricer.py

--- Custom Machine Learning Package/tests/test_neural_network_pricer.py
+++ Custom Machine Learning Package/tests/test_neural_network_pricer.py
@@ -1,14 +1,1 @@
-import numpy as np
-from airbnb_pricing_package.neural_network_pricer import NeuralNetworkPricer
 
-def test_neural_network_predict():
-    # regression test
-    X = np.array([[1], [2], [3], [4], [5]])
-    y = np.array([100, 150, 200, 250, 300])
-
-    model = NeuralNetworkPricer(hidden_layer_sizes=(10,), max_iter=500)
-    model.train(X, y)
-
-    pred = model.predict([[3.5]])
-    assert isinstance(pred[0], float), "Prediction should return a float price"
-    assert 50 < pred[0] < 400, "Prediction seems out of expected price range"
